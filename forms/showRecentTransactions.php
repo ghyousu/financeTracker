8770579b4a365cb99f42a05ebdeb2e8921201892
Mason You <masonyou.su@gmail.com>
2025-02-12 19:50:16 -0500

    updated background color

--- forms/showRecentTransactions.php
+++ forms/showRecentTransactions.php
@@ -14,7 +14,11 @@
           $transactions = getRecentTransctions(0); // 0 for bank_db_id, means all banks
       ?>
       <?php foreach ($transactions as $trans): ?>
-         <tr>
+         <?php if ($trans->amount > 0) : ?>
+         <tr class='tr_deposits'>
+         <?php else : ?>
+         <tr class='tr_withdrawals'>
+         <?php endif; ?>
             <td class='td_trans_bank_alias'> <?php echo $trans->bank_alias; ?> </td>
 
             <td class='td_trans_date'> <?php echo substr($trans->trans_date, 0, 10); ?> </td>
