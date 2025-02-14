2fb4c0c0b4c168c3c40024f91f319bb01b5b0a12
Mason You <masonyou.su@gmail.com>
2025-02-14 17:42:19 -0500

    added trigger to auto-decrement total balance on delete

--- schemas/procedures.sql
+++ schemas/procedures.sql
@@ -1,4 +1,4 @@
--- auto update total balance after each transaction
+-- auto update total balance after add/update transaction
 CREATE OR REPLACE FUNCTION uft.auto_update_balance() RETURNS TRIGGER
 AS $$
    DECLARE
@@ -13,7 +13,27 @@ AS $$
    END;
 $$ LANGUAGE 'plpgsql';
 
+-- auto update total balance after each DELETE
+CREATE OR REPLACE FUNCTION uft.auto_update_balance_on_delete() RETURNS TRIGGER
+AS $$
+   DECLARE
+      old_balance NUMERIC(10,2) := 0;
+      new_balance NUMERIC(10,2) := 0;
+   BEGIN
+      SELECT total_balance INTO old_balance FROM uft.bank WHERE bank_db_id = OLD.bank_db_id;
+      new_balance := old_balance - OLD.amount;
+
+      UPDATE uft.bank SET total_balance = new_balance WHERE bank_db_id = OLD.bank_db_id;
+      RETURN OLD;
+   END;
+$$ LANGUAGE 'plpgsql';
+
+
 CREATE OR REPLACE TRIGGER auto_update_total_balance_trigger
    AFTER INSERT OR UPDATE ON uft.transaction
       FOR EACH ROW EXECUTE FUNCTION uft.auto_update_balance();
+
+CREATE OR REPLACE TRIGGER auto_update_total_balance_on_delete_trigger
+   AFTER DELETE ON uft.transaction
+      FOR EACH ROW EXECUTE FUNCTION uft.auto_update_balance_on_delete();
 
