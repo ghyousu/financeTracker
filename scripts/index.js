154a22c1765f4ca8232a5d3a8504ce7bfb573c2a
Mason You <masonyou.su@gmail.com>
2025-04-05 18:17:13 -0400

    actually fixing the date_str in javascript this time

--- scripts/index.js
+++ scripts/index.js
@@ -35,13 +35,16 @@ function updateDatePickerValue(html_class)
    debugger ;
    var objs = document.getElementsByClassName(html_class);
 
-   const mm_array = [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" ];
+   const MM_ARRAY = [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" ];
+   const DD_ARRAY = [ "NA", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ];
 
    var local_date  = new Date();
    var yyyy = local_date.getFullYear();
-   var mm_index = local_date.getMonth();
-   var mm   = mm_array[mm_index];
-   var dd   = local_date.getDate();
+   var mm_index = local_date.getMonth(); // 0 based index, Jan returns 0, without the leading "0"
+   var dd_index = local_date.getDate();  // 1 based index, the 5th returns 5, without the leading "0"
+
+   var mm   = MM_ARRAY[mm_index];
+   var dd   = DD_ARRAY[dd_index];
    var date_str = yyyy + '-' + mm + '-' + dd;
 
    for (let i=0; i < objs.length; i++)
